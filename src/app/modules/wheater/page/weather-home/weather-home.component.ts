d213762c3147ca042efce4ff47f5f8d1e9281361
rRodrigoAM <contasloginrod@gmail.com>
2024-06-27 14:04:36 -0300

    Implementando WeatherHomeComponent e integrando WeatherService

--- src/app/modules/wheater/page/weather-home/weather-home.component.ts
+++ src/app/modules/wheater/page/weather-home/weather-home.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { WeatherService } from './../../services/weather.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
   styleUrls: []
 })
-export class WeatherHomeComponent {
+export class WeatherHomeComponent implements OnInit{
+  initialCityName = 'Blumenau'
 
+  constructor(private WeatherService: WeatherService) {}
+
+  ngOnInit(): void {
+    this.getWeatherDatas(this.initialCityName);
+  }
+
+  getWeatherDatas(cityName: string): void {
+this.WeatherService.getWeatherDatas(cityName)
+.subscribe({
+  next: (response) => {
+    console.log(response);
+  },
+  error: (error) => console.log(error),
+});
+  }
 }
--- src/app/modules/wheater/services/weather.service.ts
+++ src/app/modules/wheater/services/weather.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
+  private apiKey = 'b9e2b2dd8642f2231cd601b670b5d882';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getWeatherDatas(cityName: string): Observable<any> {
+    return this.http.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&mode=json&appid=${this.apiKey}`,
+      {}
+    );
+  }
 }
